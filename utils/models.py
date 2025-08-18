16a2443b707665d6065801937f241579adc71ccb
SaugatMgr <saugatmagar433@gmail.com>
2025-08-18 22:35:04 +0545

    fix: solve User model improperly configured error

--- utils/models.py
+++ utils/models.py
@@ -1,24 +1,22 @@
 import uuid
 
-from django.contrib.auth import get_user_model
+from django.conf import settings
 from django.db import models
 
 from utils.threads import get_current_user
-
-User = get_user_model()
 
 
 class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_by = models.ForeignKey(
-        User,
+        settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
         related_name="%(app_label)s_%(class)s_created",
     )
     modified_by = models.ForeignKey(
-        User,
+        settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
@@ -28,9 +26,11 @@ class BaseModel(models.Model):
     modified_at = models.DateTimeField("last modified at", auto_now=True)
 
     def save(self, *args, **kwargs):
+        from apps.users.models import CustomUser
+
         user = get_current_user()
 
-        if isinstance(user, User):
+        if isinstance(user, CustomUser):
             if not self.pk and not self.created_by:
                 self.created_by = user
             self.modified_by = user
