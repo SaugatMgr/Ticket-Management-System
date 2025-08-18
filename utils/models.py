b45773d4272b9145673bc88611e6db1fd910fd72
SaugatMgr <saugatmagar433@gmail.com>
2025-08-18 22:32:05 +0545

    chore: minor improvements

--- utils/models.py
+++ utils/models.py
@@ -1,22 +1,24 @@
 import uuid
 
-from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from utils.threads import get_current_user
+
+User = get_user_model()
 
 
 class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
+        User,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
         related_name="%(app_label)s_%(class)s_created",
     )
     modified_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
+        User,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
@@ -26,12 +28,10 @@ class BaseModel(models.Model):
     modified_at = models.DateTimeField("last modified at", auto_now=True)
 
     def save(self, *args, **kwargs):
-        from apps.users.models import CustomUser
-
         user = get_current_user()
 
-        if isinstance(user, CustomUser):
-            if not self.pk:
+        if isinstance(user, User):
+            if not self.pk and not self.created_by:
                 self.created_by = user
             self.modified_by = user
         else:
