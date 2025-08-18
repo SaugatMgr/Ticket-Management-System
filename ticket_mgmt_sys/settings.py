931f9a25e63f9c3e424a157108f364edc7d060ec
SaugatMgr <saugatmagar433@gmail.com>
2025-08-18 22:14:56 +0545

    chore: add ThreadMiddleware to settings MIDDLEWARE list

--- ticket_mgmt_sys/settings.py
+++ ticket_mgmt_sys/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "utils.threads.ThreadMiddleware",
 ]
 
 ROOT_URLCONF = "ticket_mgmt_sys.urls"
