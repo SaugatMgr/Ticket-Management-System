fcb14517c2dce24b3dd13f317170549e5b4e9a55
SaugatMgr <saugatmagar433@gmail.com>
2025-08-25 23:06:25 +0545

    feat, refactor: add role based permission; remove role only and hardcoded permissions

--- utils/permissions.py
+++ utils/permissions.py
@@ -3,29 +3,48 @@
 from rest_framework.permissions import BasePermission
 
 
-class HasPermission(BasePermission):
+class RolePermission(BasePermission):
     """
-    Generic permission check against user's role permissions.
+    Checks if the user's role has permission for the current action.
     """
 
-    required_permission: str = None
+    perms_map = {
+        "create": "can_create_{model}",
+        "list": "can_view_{model}",
+        "retrieve": "can_view_{model}",
+        "update": "can_edit_{model}",
+        "partial_update": "can_edit_{model}",
+        "destroy": "can_delete_{model}",
+    }
 
     def has_permission(self, request, view):
-        if not self.required_permission:
-            return True  # If no specific permission is required, allow access
+        user = request.user
+        if not user.is_authenticated:
+            return False
 
-        return request.user.is_authenticated and request.user.has_permission(
-            self.required_permission
-        )
+        if user.is_superuser:
+            return True
+
+        action = getattr(view, "action", None)
+        model_name = view.queryset.model._meta.model_name
+
+        required_perm = self.perms_map.get(action, "").format(model=model_name)
+
+        if not required_perm:
+            return False
+
+        return user.has_permission(required_perm)
 
 
-class CanManageTicket(HasPermission):
-    required_permission = "can_manage_ticket"
+class IsAdmin(BasePermission):
+    message = "Only Admin can access"
 
+    def has_permission(self, request, view):
+        user = request.user
+        user_is_active_and_authenticated = user.is_active and user.is_authenticated
 
-class CanManageTicketStatus(HasPermission):
-    required_permission = "can_manage_ticket_status"
-
-
-class CanManageTicketPriority(HasPermission):
-    required_permission = "can_manage_ticket_priority"
+        if not user_is_active_and_authenticated:
+            return False
+        if user.is_superuser:
+            return True
+        return user.role.name.lower() == "admin"
