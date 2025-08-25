6b1b8c4317117956d8119596ff26a4a2ade1507d
SaugatMgr <saugatmagar433@gmail.com>
2025-08-25 07:43:49 +0545

    refactor, fix: remove default groups, permissions from CustomUser model
    - Removed default groups and user_permissions fields from CustomUser model.
    - Removed Permission model
    - Used django contrib auth Permission model in Role model
    - Due to migrations issues, db was reset and migrations were recreated

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,21 +1,9 @@
 import uuid
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Permission
 from django.db import models
 
 from utils.models import BaseModel
-
-
-class Permission(BaseModel):
-    """
-    Represents a permission that can be assigned to roles.
-    """
-
-    codename = models.CharField(max_length=100, unique=True)
-    description = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.codename
 
 
 class Role(BaseModel):
@@ -40,6 +28,8 @@ class CustomUser(AbstractUser):
     assigned_menus = models.ManyToManyField(
         "tickets.MenuLevel3", blank=True, related_name="assigned_users"
     )
+    user_permissions = None  # Disable default user_permissions field
+    groups = None  # Disable default groups field
     created_at = models.DateTimeField("created at", auto_now_add=True, null=True)
     modified_at = models.DateTimeField("last modified at", auto_now=True, null=True)
 
