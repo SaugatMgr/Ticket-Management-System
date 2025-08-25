79eda3eca2726ab4a26c78b187cee7597258638f
SaugatMgr <saugatmagar433@gmail.com>
2025-08-25 23:09:10 +0545

    feat: add assign and unassign tickets and menus api

--- apps/tickets/api/v1/viewsets/viewsets.py
+++ apps/tickets/api/v1/viewsets/viewsets.py
@@ -1,7 +1,10 @@
+from django.contrib.contenttypes.models import ContentType
 from django.db import transaction
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from apps.notifications.models import NotificationLog
 from apps.tickets.api.v1.serializers.serializers import (
     MenuLevel1Serializer,
     MenuLevel2CreateUpdateSerializer,
@@ -22,13 +25,11 @@ from apps.tickets.models import (
     TicketPriority,
     TicketStatus,
 )
+from apps.users.models import CustomUser
+from utils.constants import NotificationLogActionChoices
 from utils.pagination import CustomPageSizePagination
-from utils.permissions import (
-    CanManageTicket,
-    CanManageTicketPriority,
-    CanManageTicketStatus,
-    HasPermission,
-)
+from utils.permissions import RolePermission
+from utils.services import get_instance_by_attr
 
 
 class TicketViewSet(ModelViewSet):
@@ -41,16 +42,12 @@ class TicketViewSet(ModelViewSet):
         "list": TicketListSerializer,
         "retrieve": TicketDetailSerializer,
     }
+    permission_classes = [RolePermission]
 
     def get_serializer_class(self):
         return self.action_serializer_mapping.get(
             self.action, super().get_serializer_class()
         )
-
-    def get_permissions(self):
-        if self.action in ["create", "update", "partial_update", "destroy"]:
-            return [CanManageTicket()]
-        return [HasPermission()]
 
     def create(self, request, *args, **kwargs):
         data = request.data
@@ -62,6 +59,63 @@ class TicketViewSet(ModelViewSet):
             ticket = serializer.save()
             return Response(data=TicketDetailSerializer(ticket).data)
 
+    @action(methods=["POST"], detail=True, url_path="assign")
+    def assign_ticket(self, request, *args, **kwargs):
+        with transaction.atomic():
+            content_type = ContentType.objects.get_for_model(Ticket)
+            ticket_id = kwargs.get("pk")
+            user_id = request.data.get("user_id")
+
+            user = get_instance_by_attr(CustomUser, "id", user_id)
+            ticket = get_instance_by_attr(Ticket, "id", ticket_id)
+
+            ticket.assigned_to = user
+            ticket.save()
+
+            NotificationLog.objects.create(
+                user=user,
+                action=NotificationLogActionChoices.ASSIGN,
+                content_type=content_type,
+                object_id=ticket.id,
+                message=f"ticket {ticket.title} assigned to User {user.get_full_name()}.",
+            )
+            print("**********Ticket Assigned Notification**********")
+            print(f"Ticket {ticket.title} assigned to User {user.get_full_name()}.")
+            print("**********Ticket Assigned Notification**********")
+
+            return Response({"message": "Ticket assigned successfully."})
+
+    @action(methods=["POST"], detail=True, url_path="unassign")
+    def unassign_ticket(self, request, *args, **kwargs):
+        with transaction.atomic():
+            content_type = ContentType.objects.get_for_model(Ticket)
+            ticket_id = kwargs.get("pk")
+            user_id = request.data.get("user_id")
+
+            user = get_instance_by_attr(CustomUser, "id", user_id)
+            ticket = get_instance_by_attr(Ticket, "id", ticket_id)
+
+            if not ticket.assigned_to or ticket.assigned_to.id != user.id:
+                return Response(
+                    {"error": "User is not assigned to this ticket."}, status=400
+                )
+
+            ticket.assigned_to = None
+            ticket.save()
+
+            NotificationLog.objects.create(
+                user=user,
+                action=NotificationLogActionChoices.UNASSIGN,
+                content_type=content_type,
+                object_id=ticket.id,
+                message=f"Removed User {user.get_full_name()} from Ticket {ticket.title}.",
+            )
+            print("**********Ticket UnAssigned Notification**********")
+            print(f"Removed User {user.get_full_name()} from Ticket {ticket.title}.")
+            print("**********Ticket UnAssigned Notification**********")
+
+            return Response({"message": "Ticket unassigned successfully."})
+
 
 class TicketStatusViewSet(ModelViewSet):
     queryset = TicketStatus.objects.all()
@@ -69,11 +123,7 @@ class TicketStatusViewSet(ModelViewSet):
     search_fields = ["name", "weight"]
     filterset_fields = ["name", "weight"]
     ordering_fields = ["name", "weight"]
-
-    def get_permissions(self):
-        if self.action in ["create", "update", "partial_update", "destroy"]:
-            return [CanManageTicketStatus()]
-        return [HasPermission()]
+    permission_classes = [RolePermission]
 
 
 class TicketPriorityViewSet(ModelViewSet):
@@ -82,22 +132,20 @@ class TicketPriorityViewSet(ModelViewSet):
     search_fields = ["name", "weight"]
     filterset_fields = ["name", "weight"]
     ordering_fields = ["name", "weight"]
-
-    def get_permissions(self):
-        if self.action in ["create", "update", "partial_update", "destroy"]:
-            return [CanManageTicketPriority()]
-        return [HasPermission()]
+    permission_classes = [RolePermission]
 
 
 class MenuLevel1ViewSet(ModelViewSet):
     queryset = MenuLevel1.objects.all()
     serializer_class = MenuLevel1Serializer
     pagination_class = CustomPageSizePagination
+    permission_classes = [RolePermission]
 
 
 class MenuLevel2ViewSet(ModelViewSet):
     queryset = MenuLevel2.objects.all()
     pagination_class = CustomPageSizePagination
+    permission_classes = [RolePermission]
 
     def get_serializer_class(self):
         if self.action in ["create", "update"]:
@@ -108,8 +156,66 @@ class MenuLevel2ViewSet(ModelViewSet):
 class MenuLevel3ViewSet(ModelViewSet):
     queryset = MenuLevel3.objects.all()
     pagination_class = CustomPageSizePagination
+    permission_classes = [RolePermission]
 
     def get_serializer_class(self):
         if self.action in ["create", "update"]:
             return MenuLevel3CreateUpdateSerializer
         return MenuLevel3ListDetailSerializer
+
+    @action(methods=["POST"], detail=True, url_path="assign")
+    def assign_menu(self, request, *args, **kwargs):
+        with transaction.atomic():
+            content_type = ContentType.objects.get_for_model(MenuLevel3)
+            menu_id = kwargs.get("pk")
+            user_id = request.data.get("user_id")
+
+            user = get_instance_by_attr(CustomUser, "id", user_id)
+            menu = get_instance_by_attr(MenuLevel3, "id", menu_id)
+
+            user.assigned_menus.add(menu)
+            user.save()
+
+            NotificationLog.objects.create(
+                user=user,
+                action=NotificationLogActionChoices.ASSIGN,
+                content_type=content_type,
+                object_id=menu.id,
+                message=f"Menu {menu.name} assigned to User {user.get_full_name()}.",
+            )
+            print("**********Menu Assigned Notification**********")
+            print(
+                f"Added Menu {menu.name} to User {user.get_full_name()} assigned menu list."
+            )
+            print("**********Menu Assigned Notification**********")
+
+            return Response({"message": "Menu assigned successfully."})
+
+    @action(methods=["POST"], detail=True, url_path="unassign")
+    def unassign_menu(self, request, *args, **kwargs):
+        with transaction.atomic():
+            content_type = ContentType.objects.get_for_model(MenuLevel3)
+            menu_id = kwargs.get("pk")
+            user_id = request.data.get("user_id")
+
+            user = get_instance_by_attr(CustomUser, "id", user_id)
+            menu = get_instance_by_attr(MenuLevel3, "id", menu_id)
+
+            user.assigned_menus.remove(menu)
+            user.save()
+
+            NotificationLog.objects.create(
+                user=user,
+                action=NotificationLogActionChoices.UNASSIGN,
+                content_type=content_type,
+                object_id=menu.id,
+                message=f"Menu {menu.name} unassigned from User {user.get_full_name()} assigned menu list.",
+            )
+
+            print("**********Menu Assigned Notification**********")
+            print(
+                f"Menu {menu.name} unassigned from User {user.get_full_name()} assigned menu list."
+            )
+            print("**********Menu Assigned Notification**********")
+
+            return Response({"message": "Menu unassigned successfully."})
